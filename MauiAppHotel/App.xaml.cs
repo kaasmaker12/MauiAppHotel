8e260f58165c976158f013cb47289ed3cb9f608a
kaasm <kaasm@PORNO-SILVA>
2025-11-02 13:04:22 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Height = 1200;
+            window.Width = 600;
+
+            return window;
+        }
+    }
+}
